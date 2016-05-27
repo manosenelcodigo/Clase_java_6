15de42e5bd2f11db719a798ad1eb66097aeb4014
#manosenelcódigo <peligrosamente@gmail.com>
2016-05-27 00:56:20 -0300

    Interface1.java

---
+++ Interface1.java
@@ -0,0 +1,10 @@
+
+package com.manosenelcodigo.interfaces;
+
+
+public interface Interface1 extends Interface2
+{
+    public String getDato();
+    public int getEntero();
+    public void ejecutar();
+}
